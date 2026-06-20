5983f2daeec0b3163f3ea83c4dca55433bc175b6
Ridvance <66145253+Ridvance@users.noreply.github.com>
2026-06-20 18:58:40 +0300

    Deploy privacy policy page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3504258190"
+    serviceWorkerVersion: "4055569253"
   }
 });
